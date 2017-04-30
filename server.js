9ceaf2d1066f6f77e16ea3efa016f0d49890bbc6
talta <t.taltallc@gmail.com>
2017-04-30 16:02:45 -0400

    logging port

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ function runServer(databaseUrl = DATABASE_URL, port=PORT){
 	return new Promise((resolve, reject)=>{
 		mongoose.connect(databaseUrl, err =>{
 			app.listen(port, () =>{
-				console.log(`your app is listening on port ${port}, and DB: ${databaseUrl}`);
+				console.log(`your app is listening on port ${port}, and process.end: ${process.env}`);
 				resolve();
 			})
 			.on('error', err =>{
