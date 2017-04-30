0aa3c06ffcb90afb5cda365fd21863f089d3991c
talta <t.taltallc@gmail.com>
2017-04-30 11:51:37 -0400

    database name to all lowercase

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 exports.DATABASE_URL = (process.env.DATABASE_URL ||
 						global.DATABASE_URL ||
-						'mongodb://tatest1:Password1234@ds149278.mlab.com:49278/quizEngine'
+						'mongodb://tatest1:Password1234@ds149278.mlab.com:49278/quizengine'
 						);
 
 exports.TEST_DATABASE_URL = (process.env.TEST_DATABASE_URL ||
