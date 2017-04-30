c4fe2f3f5876a02b0294b432c0329537bd672586
talta <t.taltallc@gmail.com>
2017-04-30 11:47:19 -0400

    database URL

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 exports.DATABASE_URL = (process.env.DATABASE_URL ||
 						global.DATABASE_URL ||
-						'mongodb://tatest:Password1234@ds149278.mlab.com:49278/quiz-engine'
+						'mongodb://tatest1:Password1234@ds149278.mlab.com:49278/quizEngine'
 						);
 
 exports.TEST_DATABASE_URL = (process.env.TEST_DATABASE_URL ||
